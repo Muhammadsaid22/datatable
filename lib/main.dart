d2c296c5f82df5ab63f671114a6b8f82f5093212
Muhammadsaid22 <82633656+Muhammadsaid22@users.noreply.github.com>
2022-05-26 18:24:38 +0500

    change main dart firebase location

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,14 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
       options: FirebaseOptions(
-          apiKey: 'AIzaSyC3PamA0LhxdGce2M464_OYqhYPyiMdkNs',
-          appId: '1:398352123113:android:9dc703bbffd1991444dc78',
-          messagingSenderId: 'messagingSenderId',
-          projectId: 'flutter-chat-36135'
+          apiKey: "AIzaSyCrDj7slJPYSuJdDwAiEvB73LuhQH0KBrA",
+          authDomain: "flutter-chat-36135.firebaseapp.com",
+          databaseURL: "https://flutter-chat-36135-default-rtdb.firebaseio.com",
+          projectId: "flutter-chat-36135",
+          storageBucket: "flutter-chat-36135.appspot.com",
+          messagingSenderId: "398352123113",
+          appId: "1:398352123113:web:c6a7dc89ff83825c44dc78",
+          measurementId: "G-Y0LNC7T5XQ"
       )
   );
   SharedPreferences pref = await SharedPreferences.getInstance();
